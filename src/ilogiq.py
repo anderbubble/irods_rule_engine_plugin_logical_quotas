ab9d2f99d33dce829306e6be753e4993f63d7e31
Jonathon Anderson <jonathon.anderson@colorado.edu>
2020-06-06 00:35:11 -0600

    Parse bytes from multiple possible units

--- src/ilogiq.py
+++ src/ilogiq.py
@@ -33,7 +33,10 @@ def main ():
     if args.operation == 'ls':
         imeta_lsw(args.collection)
     else:
-        irule(args.operation, args.collection, value=args.value, verbose=args.verbose)
+        value = args.value
+        if args.operation == 'logical_quotas_set_maximum_size_in_bytes':
+            value = parse_bytes(value)
+        irule(args.operation, args.collection, value=value, verbose=args.verbose)
 
 
 def irule (operation, collection, value=None, verbose=False):
@@ -54,6 +57,32 @@ def irule (operation, collection, value=None, verbose=False):
 def imeta_lsw(collection):
     cmd = ['imeta', 'lsw', '-C', collection, 'irods::logical_quotas::%']
     subprocess.call(cmd)
+
+
+def parse_bytes(bytes_str):
+    bytes_str = bytes_str.lower()
+    if bytes_str.endswith('pb'):
+        return int(bytes_str[:-2]) * (1000 ** 5)
+    elif bytes_str.endswith('tb'):
+        return int(bytes_str[:-2]) * (1000 ** 4)
+    elif bytes_str.endswith('gb'):
+        return int(bytes_str[:-2]) * (1000 ** 3)
+    elif bytes_str.endswith('mb'):
+        return int(bytes_str[:-2]) * (1000 ** 2)
+    elif bytes_str.endswith('kb'):
+        return int(bytes_str[:-2]) * (1000 ** 1)
+    elif bytes_str.endswith('pib'):
+        return int(bytes_str[:-3]) * (1024 ** 5)
+    elif bytes_str.endswith('tib'):
+        return int(bytes_str[:-3]) * (1024 ** 4)
+    elif bytes_str.endswith('gib'):
+        return int(bytes_str[:-3]) * (1024 ** 3)
+    elif bytes_str.endswith('mib'):
+        return int(bytes_str[:-3]) * (1024 ** 2)
+    elif bytes_str.endswith('kib'):
+        return int(bytes_str[:-3]) * (1024 ** 1)
+    else:
+        return int(bytes_str)
 
 
 if __name__ == '__main__':
